abb103132b9191b117c9660c379b33883162af9c
seanballais <sean@seanballais.com>
2016-12-17 18:48:15 +0800

    Added tiling.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <SFML/System/Vector2.hpp>
 
 #include <algorithm>
+#include <cmath>
+
+sf::Vector2i getShapePosition(int mouseX, int mouseY);
 
 int main()
 {
@@ -22,8 +25,12 @@ int main()
         }
 
         sf::Vector2i mousePos = sf::Mouse::getPosition(window);
-        mouseX = std::min(std::max((int) mousePos.x, 0), 800);
-        mouseY = std::min(std::max((int) mousePos.y, 0), 600);
+        mouseX = std::min(std::max(mousePos.x, 0), 800);
+        mouseY = std::min(std::max(mousePos.y, 0), 600);
+
+        mousePos = getShapePosition(mouseX, mouseY);
+        mouseX = mousePos.x;
+        mouseY = mousePos.y;
 
         shape.setPosition(mouseX, mouseY);
 
@@ -33,4 +40,13 @@ int main()
     }
 
     return 0;
+}
+
+sf::Vector2i getShapePosition(int mouseX, int mouseY)
+{
+    int xLowerLimit = mouseX - (mouseX % 50);
+    int yLowerLimit = mouseY - (mouseY % 50);
+
+    sf::Vector2i newMousePos(xLowerLimit + 25, yLowerLimit + 25);
+    return newMousePos;
 }
